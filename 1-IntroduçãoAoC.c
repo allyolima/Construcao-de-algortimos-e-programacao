722fba78a1094d47d5eaf214cc6f3ff21478220b
allyolima <88412655+allyolima@users.noreply.github.com>
2021-08-12 10:16:02 -0300

    1- Introdução ao C

---
+++ 1-IntroduçãoAoC.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void main () {
+/*
+
+    Tipos de variáveis (int, float, double, string, char, bool)
+    int---> 0 - 1 - 2
+    float---> 0,67 - 13,56
+    double---> 0,67 - 13,56
+    string---> "Alciomar", "alciomarholland@gmail.com"
+    char---> "SP", "M"
+    bool---> TRUE, FALSE, 0, 1
+*/
+    int num;
+
+     printf ("Digite um numero: ");
+     scanf ("%d", &num);
+
+     pintf ("o numero digitado foi %d", num )
+
+ }
